7d003e103659c5e2afbe94968104270958be1c50
Yannick Mauray <yannick.mauray@gmail.com>
2023-07-07 00:00:46 +0200

    feat: book currency

--- lib/page/books_page.dart
+++ lib/page/books_page.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'package:xpns42/model/book.dart';
 import 'package:xpns42/provider/books_provider.dart';
 import 'package:xpns42/provider/long_operation_status_provider.dart';
+import 'package:xpns42/utils/currencies.dart';
 import 'package:xpns42/utils/localization_extension.dart';
 import 'package:xpns42/widget/long_operation_indicator.dart';
 import 'package:xpns42/widget/padded_form.dart';
@@ -44,101 +46,111 @@ class BooksPage extends ConsumerWidget {
               child: Text(context.t.noBooks),
             )
           else
-            ListView.separated(
-              itemBuilder: (context, index) {
-                final book = books[index];
-
-                return Dismissible(
-                  key: ValueKey(book.id),
-                  direction: DismissDirection.endToStart,
-                  dismissThresholds: const {
-                    DismissDirection.endToStart: 0.6,
-                  },
-                  background: ColoredBox(
-                    color: Colors.red,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(right: 24),
-                          child: Text(
-                            context.t.deleteBook,
-                            style: Theme.of(context)
-                                .textTheme
-                                .titleMedium!
-                                .copyWith(color: Colors.white),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  confirmDismiss: (direction) {
-                    return showDialog<bool>(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                          title: Text(context.t.deleteBook),
-                          content: Text(context.t.deleteBookConfirmation),
-                          actions: [
-                            TextButton(
-                              onPressed: () => Navigator.of(context).pop(false),
-                              child: Text(context.t.cancel),
-                            ),
-                            TextButton(
-                              onPressed: () => Navigator.of(context).pop(true),
-                              child: Text(context.t.delete),
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  },
-                  onDismissed: (direction) async {
-                    longOperationStatus.start();
-                    await ref.read(booksProvider.notifier).remove(book);
-                    longOperationStatus.stop();
-                  },
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
-                    child: Row(
-                      children: [
-                        const Icon(Icons.book_outlined, size: 48),
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 24),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Text(
-                                  book.title,
-                                  style:
-                                      Theme.of(context).textTheme.titleMedium,
-                                ),
-                                Text(
-                                  '${book.firstPerson} & ${book.secondPerson}',
-                                  style:
-                                      Theme.of(context).textTheme.labelMedium,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        Text(
-                          book.balance.toStringAsFixed(2),
-                          style: Theme.of(context).textTheme.titleMedium,
-                        ),
-                      ],
-                    ),
-                  ),
-                );
+            RefreshIndicator(
+              onRefresh: () async {
+                await ref.read(booksProvider.notifier).loadBooks();
               },
-              separatorBuilder: (context, index) => const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 8),
-                child: Divider(),
+              child: ListView.separated(
+                physics: const AlwaysScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  final book = books[index];
+
+                  return Dismissible(
+                    key: ValueKey(book.id),
+                    direction: DismissDirection.endToStart,
+                    dismissThresholds: const {
+                      DismissDirection.endToStart: 0.6,
+                    },
+                    background: ColoredBox(
+                      color: Colors.red,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(right: 24),
+                            child: Text(
+                              context.t.deleteBook,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleMedium!
+                                  .copyWith(color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    confirmDismiss: (direction) {
+                      return showDialog<bool>(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text(context.t.deleteBook),
+                            content: Text(context.t.deleteBookConfirmation),
+                            actions: [
+                              TextButton(
+                                onPressed: () =>
+                                    Navigator.of(context).pop(false),
+                                child: Text(context.t.cancel),
+                              ),
+                              TextButton(
+                                onPressed: () =>
+                                    Navigator.of(context).pop(true),
+                                child: Text(context.t.delete),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    onDismissed: (direction) async {
+                      longOperationStatus.start();
+                      await ref.read(booksProvider.notifier).remove(book);
+                      longOperationStatus.stop();
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 24,
+                        vertical: 8,
+                      ),
+                      child: Row(
+                        children: [
+                          const Icon(Icons.book_outlined, size: 48),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 24),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Text(
+                                    book.title,
+                                    style:
+                                        Theme.of(context).textTheme.titleMedium,
+                                  ),
+                                  Text(
+                                    '${book.firstPerson} & ${book.secondPerson}',
+                                    style:
+                                        Theme.of(context).textTheme.labelMedium,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Text(
+                            book.balance.toStringAsFixed(2),
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) => const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+                  child: Divider(),
+                ),
+                itemCount: books.length,
               ),
-              itemCount: books.length,
             ),
           const LongOperationIndicator(),
         ],
@@ -155,7 +167,12 @@ class _AddBookDialog extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Dialog(
+    final locale = Localizations.localeOf(context);
+    final format = NumberFormat.simpleCurrency(locale: locale.toString());
+    debugPrint('CURRENCY SYMBOL ${format.currencySymbol}'); // $
+    debugPrint('CURRENCY NAME ${format.currencyName}'); // USD
+
+    return Dialog.fullscreen(
       child: Scaffold(
         appBar: AppBar(
           title: Text(context.t.newBook),
@@ -173,6 +190,12 @@ class _AddBookDialog extends ConsumerWidget {
                 labelText: context.t.otherPerson,
               ),
               controller: otherPersonController,
+            ),
+            DropdownButtonFormField(
+              items: currencies,
+              hint: Text(context.t.currency),
+              value: format.currencyName,
+              onChanged: (value) {},
             ),
             ElevatedButton(
               onPressed: () {
